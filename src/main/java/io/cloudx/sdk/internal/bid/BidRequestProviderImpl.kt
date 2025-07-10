918daeb3d144bc0186f2cec82995baa9a681c528
Mirmuhsin Sodiqov <mirmukhsin.sodikov@xenoss.io>
2025-07-10 14:54:00 +0500

    - removed the old KeyValue pairs

--- src/main/java/io/cloudx/sdk/internal/bid/BidRequestProviderImpl.kt
+++ src/main/java/io/cloudx/sdk/internal/bid/BidRequestProviderImpl.kt
@@ -171,36 +171,6 @@ internal class BidRequestProviderImpl(
                 putRegsObject(privacyService)
 
                 put("ext", JSONObject().apply {
-                    put("prebid", JSONObject().apply {
-                        put("adservertargeting", JSONArray().apply {
-                            val loopIndex = PlacementLoopIndexTracker.getCount(params.placementName)
-                            put(JSONObject().apply {
-                                put("key", "loop-index")
-                                put("source", "bidrequest")
-                                put("value", loopIndex.toString())
-                            })
-                            put(JSONObject().apply {
-                                put("key", "geo")
-                                put("source", "bidrequest")
-                                put("value", "USA")
-                            })
-                            put(JSONObject().apply {
-                                put("key", "age")
-                                put("source", "bidrequest")
-                                put("value", "30")
-                            })
-
-                            if (SdkKeyValueState.userKeyValues.isNotEmpty()) {
-                                SdkKeyValueState.userKeyValues.forEach { (k, v) ->
-                                    put(JSONObject().apply {
-                                        put("key", k)
-                                        put("source", "bidrequest")
-                                        put("value", v)
-                                    })
-                                }
-                            }
-                        })
-                    })
                     putBidRequestAdapterExtras(context, bidRequestExtrasProviders)
                 })
             }
