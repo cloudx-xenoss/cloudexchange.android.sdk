d410270ce052034a7cfd947f686f4d3e69875a6d
Mirmuhsin Sodiqov <mirmukhsin.sodikov@xenoss.io>
2025-07-18 20:48:59 +0500

    Update EventBulkRequestToJson.kt

--- src/main/java/io/cloudx/sdk/internal/imp_tracker/bulk/EventBulkRequestToJson.kt
+++ src/main/java/io/cloudx/sdk/internal/imp_tracker/bulk/EventBulkRequestToJson.kt
@@ -2,6 +2,7 @@ package io.cloudx.sdk.internal.imp_tracker.bulk
 
 import org.json.JSONArray
 import org.json.JSONObject
+import java.net.URLEncoder
 
 internal fun List<EventAM>.toJson(): String {
     val itemsArray = JSONArray()
@@ -9,10 +10,10 @@ internal fun List<EventAM>.toJson(): String {
     for (item in this) {
         val itemObj = JSONObject().apply {
             put("eventName", item.eventName)
-            put("campaignId", item.campaignId)
+            put("campaignId", URLEncoder.encode(item.campaignId, "UTF-8"))
             put("eventValue", item.eventValue)
             put("type", item.type)
-            put("impression", item.impression)
+            put("impression", URLEncoder.encode(item.impression, "UTF-8"))
         }
         itemsArray.put(itemObj)
     }
